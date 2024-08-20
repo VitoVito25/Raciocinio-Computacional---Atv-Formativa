d8412e30c52c45a72be5732d97f1afde7ce4e7b0
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-08-20 20:42:18 -0300

    testando 1

--- codigo.py
+++ codigo.py
@@ -1,17 +1,4 @@
-# Estrutura menu principal
-print("------ MENU PRINCIPAL ------\n")
-print("(1) Gerenciar Estudantes")
-print("(2) Gerenciar Professores")
-print("(3) Gerenciar Disciplinas")
-print("(4) Gerenciar Turmas")
-print("(5) Gerenciar Matriculas")
-print("(9) Sair")
-
-# Solicita que o usuário insira uma opção
-gerTipo = int(input("Informe a opção desejada: "))
-
-
-# Dicionário para mapear opções a descrições menu principal
+# Dicionário para mapear opções a descrições do menu principal
 gerOpcoes = {
     1: "Estudantes",
     2: "Professores",
@@ -21,35 +8,81 @@ gerOpcoes = {
     9: "Sair"
 }
 
-# Obtém o valor correspondente ou define como "opção inválida"
-gerDesc = gerOpcoes.get(gerTipo, "Opção inválida")
-
-# Verifica se a opção é inválida
-if gerTipo < 1 or (gerTipo > 5 and gerTipo != 9):
-    print("----- [{}] VOLTANDO AO MENU PRINCIPAL... -----\n".format(gerDesc).upper())
-
-# Estrutura menu secundario
-print("\n ***** [{}] MENU DE OPERAÇÕES *****\n".format(gerDesc).upper())
-print("(1) Incluir")
-print("(2) Listar")
-print("(3) Atualizar")
-print("(4) Excluir")
-print("(9) Voltar ao menu principal")
-
-# Solicita que o usuário insira uma opção
-secTipo = int(input("Informe a opção desejada: "))
-
-# Dicionário para mapear opções a descrições menu secundario
+# Dicionário para mapear opções a descrições do menu secundário
 secOpcoes = {
     1: "Incluir",
     2: "Listar",
     3: "Atualizar",
     4: "Excluir",
-    9: "Sair"
+    9: "Voltar ao menu principal"
 }
 
-# Obtém o valor correspondente ou define como "opção inválida"
-secDesc = secOpcoes.get(secTipo, "Opção inválida")
+## Menu ##
+while True:
+    # Estrutura menu principal
+    print("------ MENU PRINCIPAL ------\n")
+    print("(1) Gerenciar Estudantes")
+    print("(2) Gerenciar Professores")
+    print("(3) Gerenciar Disciplinas")
+    print("(4) Gerenciar Turmas")
+    print("(5) Gerenciar Matriculas")
+    print("(9) Sair")
 
-#Informando Seleção
-print("\n ***** [{}] *****\n".format(secDesc).upper())
+    # Solicita que o usuário insira uma opção
+    gerTipo = int(input("Informe a opção desejada: "))
+
+    # Obtém o valor correspondente ou define como "opção inválida"
+    gerDesc = gerOpcoes.get(gerTipo, "Opção inválida")
+
+    # Verifica se a opção é válida
+    if gerTipo not in gerOpcoes:
+        print("----- [{}] VOLTANDO AO MENU PRINCIPAL... -----\n".format(gerDesc).upper())
+        continue
+
+    # Verifica se o usuário escolheu sair
+    if gerTipo == 9:
+        print("----- SAINDO DO SISTEMA... -----")
+        break
+
+    # Estrutura menu secundário
+    while True:
+        print("\n ***** [{}] MENU DE OPERAÇÕES *****\n".format(gerDesc).upper())
+        print("(1) Incluir")
+        print("(2) Listar")
+        print("(3) Atualizar")
+        print("(4) Excluir")
+        print("(9) Voltar ao menu principal")
+
+        # Solicita que o usuário insira uma opção
+        secTipo = int(input("Informe a opção desejada: "))
+
+        # Obtém o valor correspondente ou define como "opção inválida"
+        secDesc = secOpcoes.get(secTipo, "Opção inválida")
+
+        # Informando seleção
+        print("\n ***** [{}] *****\n".format(secDesc).upper())
+
+        # Verifica se a opção é válida
+        if secTipo not in secOpcoes:
+            print("----- [{}] VOLTANDO AO MENU DE OPERAÇÕES... -----\n".format(secDesc).upper())
+            continue
+
+        # Verifica se o usuário escolheu voltar ao menu principal
+        if secTipo == 9:
+            print("----- VOLTANDO AO MENU PRINCIPAL... -----")
+            break
+
+        # Utilizando match-case para substituir if-elif
+        match secTipo:
+            case 1:
+                print("Incluir operação")
+            case 2:
+                print("Listar operação")
+            case 3:
+                print("Atualizar operação")
+            case 4:
+                print("Excluir operação")
+            case _:
+                print("Opção inválida")  # Caso não necessário devido à verificação anterior
+
+        # Após a operação, o menu secundário será exibido novamente até que o usuário escolha a opção 9.
