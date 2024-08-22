532cc84423507a8f2c6fb40fba8b39006f12bcea
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-08-22 14:55:49 -0300

    validando segundo menu e clear consoles

--- codigo.py
+++ codigo.py
@@ -1,3 +1,5 @@
+from utils import clear_console
+
 # Dicionário para mapear opções a descrições do menu principal
 gerOpcoes = {
     1: "Estudantes",
@@ -18,17 +20,17 @@ secOpcoes = {
 }
 
 ## Main ##
+clear_console()
 while True:
-    # Estrutura menu principal
-    print("------ MENU PRINCIPAL ------\n")
-    print("(1) Gerenciar Estudantes")
-    print("(2) Gerenciar Professores")
-    print("(3) Gerenciar Disciplinas")
-    print("(4) Gerenciar Turmas")
-    print("(5) Gerenciar Matriculas")
-    print("(9) Sair")
-
     while True:
+        # Estrutura menu principal
+        print("------ MENU PRINCIPAL ------\n")
+        print("(1) Gerenciar Estudantes")
+        print("(2) Gerenciar Professores")
+        print("(3) Gerenciar Disciplinas")
+        print("(4) Gerenciar Turmas")
+        print("(5) Gerenciar Matriculas")
+        print("(9) Sair")
         try:
             # Solicita que o usuário insira uma opção
             gerTipo = int(input("Informe a opção desejada: "))
@@ -38,52 +40,73 @@ while True:
 
             # Verifica se a opção é válida
             if gerTipo not in gerOpcoes:
+                clear_console()
                 print("----- [{}] VOLTANDO AO MENU PRINCIPAL... -----\n".format(gerDesc).upper())
                 continue
 
             # Verifica se o usuário escolheu sair
             if gerTipo == 9:
+                clear_console()
                 print("----- SAINDO DO SISTEMA... -----")
                 break
             
             # Se o gerTipo é válido e não é 9, sai do loop
             break
 
+        #Se informar algo != int informa mensagem
         except ValueError:
+            clear_console()
             print("----- ENTRADA INVÁLIDA! POR FAVOR, INSIRA UM NÚMERO. -----")
 
     if gerTipo == 9:
         break  # Sai do loop externo
 
+    clear_console()    
     # Estrutura menu secundário
     while True:
-        print("\n ***** [{}] MENU DE OPERAÇÕES *****\n".format(gerDesc).upper())
-        print("(1) Incluir")
-        print("(2) Listar")
-        print("(3) Atualizar")
-        print("(4) Excluir")
-        print("(9) Voltar ao menu principal")
+        while True:
+            print("\n ***** [{}] MENU DE OPERAÇÕES *****\n".format(gerDesc).upper())
+            print("(1) Incluir")
+            print("(2) Listar")
+            print("(3) Atualizar")
+            print("(4) Excluir")
+            print("(9) Voltar ao menu principal")
+            try:
+                # Solicita que o usuário insira uma opção
+                secTipo = int(input("Informe a opção desejada: "))
 
-        # Solicita que o usuário insira uma opção
-        secTipo = int(input("Informe a opção desejada: "))
+                # Obtém o valor correspondente ou define como "opção inválida"
+                secDesc = secOpcoes.get(secTipo, "Opção inválida")
 
-        # Obtém o valor correspondente ou define como "opção inválida"
-        secDesc = secOpcoes.get(secTipo, "Opção inválida")
+                # Verifica se a opção é válida
+                if secTipo not in secOpcoes:
+                    clear_console()
+                    print("----- [{}] VOLTANDO AO MENU DE OPERAÇÕES... -----\n".format(secDesc).upper())
+                    continue
 
-        # Informando seleção
-        print("\n ***** [{}] *****\n".format(secDesc).upper())
+                # Verifica se o usuário escolheu voltar ao menu principal
+                if secTipo == 9:
+                    clear_console()
+                    print("----- VOLTANDO AO MENU PRINCIPAL... -----")
+                    break
+                
+                # Se o secTipo é válido e não é 9, sai do loop
+                break
 
-        # Verifica se a opção é válida
-        if secTipo not in secOpcoes:
-            print("----- [{}] VOLTANDO AO MENU DE OPERAÇÕES... -----\n".format(secDesc).upper())
-            continue
+            #Se informar algo != int informa mensagem    
+            except ValueError:    
+                clear_console()
+                print("----- ENTRADA INVÁLIDA! POR FAVOR, INSIRA UM NÚMERO. -----")
 
-        # Verifica se o usuário escolheu voltar ao menu principal
+        # Se o secTipo é válido e não é 9, loop
         if secTipo == 9:
-            print("----- VOLTANDO AO MENU PRINCIPAL... -----")
-            break
+            break  # Sai do loop externo        
 
-        # Utilizando match-case para substituir if-elif
+        #Informa a abertura do segundo menu
+        clear_console()
+        print("----- {} {} -----\n".format(secDesc, gerDesc).upper())
+
+        #Operações de fato
         match secTipo:
             case 1:
                 print("Incluir operação")
