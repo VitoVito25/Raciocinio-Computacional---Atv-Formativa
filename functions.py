808c131324fa595f3ee74dc7b4bc3a818fd3abec
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-08-28 15:35:48 -0300

    modularizando com funções

---
+++ functions.py
@@ -0,0 +1,296 @@
+from utils import clear_console
+
+# Dicionário para mapear opções a descrições do menu principal
+gerOpcoes = {
+    1: "Estudantes",
+    2: "Professores",
+    3: "Disciplinas",
+    4: "Turmas",
+    5: "Matriculas",
+    9: "Sair"
+}
+
+# Dicionário para mapear opções a descrições do menu secundário
+secOpcoes = {
+    1: "Incluir",
+    2: "Listar",
+    3: "Atualizar",
+    4: "Excluir",
+    9: "Voltar ao menu principal"
+}
+
+# Estrutura de listagem de estudantes
+estudantes = []
+
+def menuPrincipal():
+    """
+        Função para chamar o menu principal
+
+        :param: Não é necessario a inclusão de parametros
+        :return gerTipo: Retorna o int para a identificação de qual o menu selecionado
+        :return gerDesc: Retorna o descritivo para a identificação de qual o menu selecionado
+    """
+
+    while True:
+        # Estrutura menu principal
+        print("------ MENU PRINCIPAL ------\n")
+        print("(1) Gerenciar Estudantes")
+        print("(2) Gerenciar Professores")
+        print("(3) Gerenciar Disciplinas")
+        print("(4) Gerenciar Turmas")
+        print("(5) Gerenciar Matriculas")
+        print("(9) Sair")
+        try:
+            # Solicita que o usuário insira uma opção
+            gerTipo = int(input("Informe a opção desejada: "))
+
+            # Obtém o valor correspondente ou define como "opção inválida"
+            gerDesc = gerOpcoes.get(gerTipo, "Opção inválida")
+
+            # Verifica se a opção é válida
+            if gerTipo not in gerOpcoes:
+                clear_console()
+                print("----- [{}] VOLTANDO AO MENU PRINCIPAL... -----\n".format(gerDesc).upper())
+                continue
+
+            # Verifica se o usuário escolheu sair
+            if gerTipo == 9:
+                clear_console()
+                print("----- SAINDO DO SISTEMA... -----")
+                break
+            
+            # Se o gerTipo é válido e não é 9, sai do loop
+            break
+
+        #Se informar algo != int informa mensagem
+        except ValueError:
+            clear_console()
+            print("----- ENTRADA INVÁLIDA! POR FAVOR, INSIRA UM NÚMERO. -----")
+
+    return gerTipo , gerDesc
+
+
+def menuSecundario(gerDesc):
+    """
+        Função para chamar o menu secundario
+
+        :param: Insira o Descritivo do item selecionado no menu principal
+        :return secTipo: Retorna o int para a identificação de qual o menu selecionado
+        :return secDesc: Retorna o descritivo para a identificação de qual o menu selecionado
+    """
+    while True:
+        print("\n ***** [{}] MENU DE OPERAÇÕES *****\n".format(gerDesc).upper())
+        print("(1) Incluir")
+        print("(2) Listar")
+        print("(3) Atualizar")
+        print("(4) Excluir")
+        print("(9) Voltar ao menu principal")
+        try:
+            # Solicita que o usuário insira uma opção
+            secTipo = int(input("Informe a opção desejada: "))
+
+            # Obtém o valor correspondente ou define como "opção inválida"
+            secDesc = secOpcoes.get(secTipo, "Opção inválida")
+
+            # Verifica se a opção é válida
+            if secTipo not in secOpcoes:
+                clear_console()
+                print("----- [{}] VOLTANDO AO MENU DE OPERAÇÕES... -----\n".format(secDesc).upper())
+                continue
+
+            # Verifica se o usuário escolheu voltar ao menu principal
+            if secTipo == 9:
+                clear_console()
+                print("----- VOLTANDO AO MENU PRINCIPAL... -----")
+                break
+            
+            # Se o secTipo é válido e não é 9, sai do loop
+            break
+
+        #Se informar algo != int informa mensagem    
+        except ValueError:    
+            clear_console()
+            print("----- ENTRADA INVÁLIDA! POR FAVOR, INSIRA UM NÚMERO. -----")
+        
+    return secTipo, secDesc
+
+def incluirEstudantes():
+    """
+        Função para incluir estudantes
+
+        :param: Não é necessario a inclusão de parametros
+        :return: Não retorna dados
+    """
+    nome = input("Insira o nome do estudante: ")
+    cpf = input("Insira o CPF do estudante: ")
+    codigo = len(estudantes) + 1  # Gera um código sequencial para o estudante
+    estudante = {"codigo": codigo, "nome": nome, "cpf": cpf}
+    estudantes.append(estudante)
+
+    clear_console()
+    print(f"Código: {codigo}, Nome: {nome}, CPF: {cpf}")
+    print("*** Estudante inserido com Sucesso! ***")
+    input("Pressione ENTER para continuar...")
+
+def listarEstudantes():
+    """
+        Função para listar estudantes
+
+        :param: Não é necessario a inclusão de parametros
+        :return: Não retorna dados
+    """
+    if not estudantes:
+        print("A lista de estudantes está vazia.\n")
+    else:
+        print("Lista de estudantes:")
+        for estudante in estudantes:
+            print(f"Código: {estudante['codigo']}, Nome: {estudante['nome']}, CPF: {estudante['cpf']}")
+    input("Pressione ENTER para retornar ao Menu...")
+    clear_console()
+
+def alterarEstudantes(gerDesc, secDesc):
+    """
+        Função para alterar estudantes
+
+        :param gerDesc: Descritivo do menu principal
+        :param secDesc: Descritivo do menu secundario
+        :return: Não retorna dados
+    """
+
+    # Verifica se a lista de estudantes está vazia
+    if not estudantes:
+        print("A lista de estudantes está vazia.\n")
+        input("Pressione ENTER para continuar...")
+        clear_console()
+    else:
+        while True:
+            try:
+                # Insira o código do estudante a ser editado
+                codigo_edicao = int(input("Insira o Código do Estudante que será editado: "))
+
+                # Procura o estudante com o código informado
+                estudante_encontrado = None
+                for estudante in estudantes:
+                    if estudante["codigo"] == codigo_edicao:
+                        estudante_encontrado = estudante
+
+                        # Armazena os dados antigos para comparação posterior
+                        dados_anteriores = estudante_encontrado.copy()
+                        break
+                
+                # Permite que o usuário atualize o nome, CPF e/ou código
+                if estudante_encontrado:
+                    while True:
+                        #Validacao do novo codigo
+                        try:
+                            print(f"Estudante encontrado: Código: {estudante_encontrado['codigo']}, Nome: {estudante_encontrado['nome']}, CPF: {estudante_encontrado['cpf']}")
+                            novo_codigo = input(f"Insira o novo código (ou pressione ENTER para manter '{estudante_encontrado['codigo']}'): ")
+                            
+                            if novo_codigo == "":  # Se o usuário pressionar ENTER, mantém o código atual
+                                break
+                            
+                            novo_codigo = int(novo_codigo)  # Tenta converter a entrada para inteiro
+
+                            # Verifica se o novo código já está em uso
+                            codigo_existente = any(estudante['codigo'] == novo_codigo for estudante in estudantes)
+                            
+                            if codigo_existente:
+                                print(f"*** O código {novo_codigo} já está em uso. Por favor, escolha um código diferente. ***")
+                                input("Pressione ENTER para tentar novamente...")
+                                clear_console()
+                                print("----- {} {} -----\n".format(secDesc, gerDesc).upper())
+                            else:
+                                estudante_encontrado['codigo'] = novo_codigo  # Atualiza o código do estudante
+                                break
+                            
+                        except ValueError:
+                            clear_console()
+                            print("----- ENTRADA INVÁLIDA! POR FAVOR, INSIRA UM NÚMERO. -----")
+                            input("Pressione ENTER para tentar novamente...")
+                            clear_console()
+                            print("----- {} {} -----\n".format(secDesc, gerDesc).upper())                                        
+                    
+                    # Solicita novos Nome e CPF ou mantém os atuais se o usuário não informar nada
+                    novo_nome = input(f"Insira o novo nome (ou pressione ENTER para manter '{estudante_encontrado['nome']}'): ")
+                    novo_cpf = input(f"Insira o novo CPF (ou pressione ENTER para manter '{estudante_encontrado['cpf']}'): ")
+
+                    if novo_nome:
+                        estudante_encontrado['nome'] = novo_nome
+                    if novo_cpf:
+                        estudante_encontrado['cpf'] = novo_cpf
+
+                    # Exibe os dados anteriores e os dados atualizados
+                    clear_console()
+                    print("\nDados anteriores:")
+                    print(f"Código: {dados_anteriores['codigo']}, Nome: {dados_anteriores['nome']}, CPF: {dados_anteriores['cpf']}")
+                    print("\nDados atualizados:")
+                    print(f"Código: {estudante_encontrado['codigo']}, Nome: {estudante_encontrado['nome']}, CPF: {estudante_encontrado['cpf']}")
+
+                    print(f"\n*** Estudante com código {codigo_edicao} atualizado com sucesso! ***")
+                    input("Pressione ENTER para continuar...")
+                    clear_console()
+                    break
+                else:
+                    # Informa caso não tenha o estudante com o código informado
+                    clear_console()
+                    print(f"*** Nenhum estudante encontrado com o código {codigo_edicao}. ***")
+                    input("Pressione ENTER para continuar...")
+                    clear_console()
+                    print("----- {} {} -----\n".format(secDesc, gerDesc).upper())
+
+            # Trata o erro caso o usuário não informe um número inteiro
+            except ValueError:
+                clear_console()
+                print("----- ENTRADA INVÁLIDA! POR FAVOR, INSIRA UM NÚMERO. -----")
+                input("Pressione ENTER para continuar...")
+                clear_console()
+                print("----- {} {} -----\n".format(secDesc, gerDesc).upper())
+
+
+def excluirEstudantes(gerDesc, secDesc):
+    """
+        Função para excluir estudantes
+
+        :param gerDesc: Descritivo do menu principal
+        :param secDesc: Descritivo do menu secundario
+        :return: Não retorna dados
+    """
+    #Verifica se a lista de Estudantes esta vazia
+    if not estudantes:
+        print("A lista de estudantes está vazia.\n")
+        input("Pressione ENTER para continuar...")
+        clear_console()
+    else:
+        while True:
+            try:
+                #Insira o codigo do estudante a ser excluido
+                codigo_exclusao = int(input("Insira o Codigo do Estudante que será excluído: "))
+
+                # Procura o estudante com o código informado
+                estudante_encontrado = None
+                for estudante in estudantes:
+                    if estudante["codigo"] == codigo_exclusao:
+                        estudante_encontrado = estudante
+                        break
+
+                if estudante_encontrado:
+                    clear_console()
+                    estudantes.remove(estudante_encontrado)
+                    print(f"*** Estudante com código {codigo_exclusao} excluído com sucesso! ***")
+                    input("Pressione ENTER para continuar...")
+                    clear_console()
+                    break 
+                else:
+                    #Informa caso nao tenha o estudante com o codigo informado
+                    clear_console()
+                    print(f"*** Nenhum estudante encontrado com o código {codigo_exclusao}. ***")
+                    input("Pressione ENTER para continuar...")
+                    clear_console()
+                    print("----- {} {} -----\n".format(secDesc, gerDesc).upper())
+
+            #Trata o erro caso o usuario nao informe um int
+            except ValueError:
+                print("----- ENTRADA INVÁLIDA! POR FAVOR, INSIRA UM NÚMERO. -----")
+                input("Pressione ENTER para continuar...")
+                clear_console()
+                print("----- {} {} -----\n".format(secDesc, gerDesc).upper())
---
+++ main.py
@@ -0,0 +1,105 @@
+from utils import clear_console
+from functions import *
+
+# Teste
+"""
+estudante = {"codigo": 1, "nome": 1, "cpf": 1}
+estudantes.append(estudante)
+estudante = {"codigo": 2, "nome": 2, "cpf": 2}
+estudantes.append(estudante)
+estudante = {"codigo": 3, "nome": 3, "cpf": 3}
+estudantes.append(estudante)
+"""
+
+## Main App ##
+clear_console()
+while True:
+
+    #Chama o menu principal e obtem o numero e descritivo do menu selecionado
+    gerTipo, gerDesc = menuPrincipal()
+
+    if gerTipo == 9:
+        break  # Sai do loop externo
+
+    clear_console()    
+    # Estrutura menu secundário
+    while True:
+        
+        #Chama o menu secundario e obtem o numero e descritivo do menu selecionado
+        secTipo, secDesc = menuSecundario(gerDesc)
+
+        # Se o secTipo é válido e não é 9, loop
+        if secTipo == 9:
+            break  # Sai do loop externo        
+
+        #Informa a abertura do menu
+        clear_console()
+        print("----- {} {} -----\n".format(secDesc, gerDesc).upper())
+
+        match gerTipo:
+            #Estudantes
+            case 1 : 
+                match secTipo:
+                    case 1:
+                        incluirEstudantes()   
+                    case 2:
+                        listarEstudantes()    
+                    case 3:
+                        alterarEstudantes(gerDesc, secDesc)
+                    case 4:
+                        excluirEstudantes(gerDesc, secDesc)
+            case 2 :
+                match secTipo:
+                    case 1:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break
+                    case 2:
+                        print("----- EM DESENVOLVIMENTO -----")
+                    case 3:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break
+                    case 4:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break        
+            case 3 :
+                match secTipo:
+                    case 1:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break
+                    case 2:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break
+                    case 3:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break
+                    case 4:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break
+            case 4 :
+                match secTipo:
+                    case 1:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break
+                    case 2:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break
+                    case 3:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break
+                    case 4:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break            
+            case 5 :
+                match secTipo:
+                    case 1:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break
+                    case 2:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break
+                    case 3:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break
+                    case 4:
+                        print("----- EM DESENVOLVIMENTO -----")
+                        break
